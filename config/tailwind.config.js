a63c038aafe6b1e8835f3da0f231d4348362662a
Cameron Domotor <33791546+cdomotor@users.noreply.github.com>
2025-08-19 05:52:55 +1000

    STABLE 20250819_0552

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,12 @@
+// File: tailwind.config.js
+// Path: /tailwind.config.js
 module.exports = {
+  darkMode: 'class', // use the 'dark' class on <html>
   content: [
+    './app/views/**/*.erb',
     './app/helpers/**/*.rb',
-    './app/views/**/*',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
   ],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+  theme: { extend: {} },
+  plugins: [],
 }
